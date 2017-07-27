bbc45626352a14a322931b634f32a6c27bc09ade
getmouth <emmopk11@outlook.com>
2017-07-28 00:39:42 +0200

    4.1 scaffold product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :products
+
   root 'static_pages#index'
+
+  get '/products', to:'products#show'
+
+  get  '/about',   to: 'static_pages#about'
+
+
+  get  '/contact', to: 'static_pages#contact'
+
+  get  'index',    to: 'static_pages#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end 
