4916d48c1d2856ddfd4a82be853563b8c6190d42
getmouth <emmopk11@outlook.com>
2017-08-12 06:04:08 +0200

    5.2 Forms database error fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812015448) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20170812030557) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -30,7 +27,7 @@ ActiveRecord::Schema.define(version: 20170812015448) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.integer "price"
+    t.float "price"
   end
 
 end
