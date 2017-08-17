909f524876b0d59af88a5e23ac06dedee793adda
getmouth <emmopk11@outlook.com>
2017-08-17 02:27:49 +0200

    userclass update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource
-  before_filter :authenticate_user!
   before_action :authenticate_user!, except: [:show, :index]
 
   # GET /users
