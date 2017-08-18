ce767970541763a038182cf12a8945973a9343df
getmouth <emmopk11@outlook.com>
2017-08-18 04:57:12 +0200

    5.7 comments added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,20 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
   def self.search(search_term)
-   if Rails.env.development?
-    Product.where("name LIKE ?", "%#{search_term}%")
-  else
-    Product.where("name ilike ?", "%#{search_term}%")
+    if Rails.env.development?
+      Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      Product.where("name ilike ?", "%#{search_term}%")
+    end
   end
-end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815124216) do
+ActiveRecord::Schema.define(version: 20170817190918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "body"
+    t.integer "rating"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -52,4 +63,6 @@ ActiveRecord::Schema.define(version: 20170815124216) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
 end
