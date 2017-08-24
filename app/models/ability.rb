ccafbfba1db5b78d0b83243b5489086535fccffd
getmouth <emmopk11@outlook.com>
2017-08-24 14:50:55 +0200

    cancan fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,9 @@ class Ability
   
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    # if user.admin?
-    #   can :destroy, Comment
-    # else
+    if user.admin?
+      can :destroy, Comment
+    else
     can :manage, User, id: user.id
     # Define abilities for the passed in user here. For example:
     #
