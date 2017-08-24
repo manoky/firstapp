305154466455f496c82b681842ee676da80e9efb
getmouth <emmopk11@outlook.com>
2017-08-24 15:52:48 +0200

    admin roles error fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
       can :manage, :all
     else
     can :manage, User, id: user.id
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
