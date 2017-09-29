415ffd3cc57bfd0f18f411957183ea75d37b8eb9
getmouth <emmopk11@outlook.com>
2017-09-29 22:13:01 +0200

    6.12 redis heroku fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,6 @@
-if Rails.env.development?
-  $redis = Redis.new(:host => 'localhost', :port => 6379)
-
-else Rails.env.production?
+if Rails.env.production?
   $redis = Redis.new(url: ENV["REDIS_URL"])
 
+ else 
+  $redis = Redis.new(:host => 'localhost', :port => 6379)
 end
